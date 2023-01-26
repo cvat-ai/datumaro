3ad4cc5be9b3ec6c3efc47124a7d91f6d0c432f9
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2023-01-26 22:19:54 +0200

    t

--- tests/test_reproduce_bug_macos.py
+++ tests/test_reproduce_bug_macos.py
@@ -23,7 +23,7 @@ def download_model(files):
             f.write(requests.get(url).content)
 
 def test_macos_bug():
-    download_model()
+    download_model(download_files)
 
     proto = prototxt_file_name
     model = caffemodel_file_name
@@ -34,14 +34,27 @@ def test_macos_bug():
     net.getLayer(net.getLayerId("class8_ab")).blobs = [pts_in_hull]
     net.getLayer(net.getLayerId("conv8_313_rh")).blobs = [np.full([1, 313], 2.606, np.float32)]
 
-    frame = np.ones((224, 224))
+    frame = np.ones((224, 224), dtype=np.float32)
+    # frame = np.ones((24, 36), dtype=np.uint8)
 
-    H_orig, W_orig = frame.shape[:2]
-    if len(frame.shape) == 2 or frame.shape[-1] == 1:
-        frame = np.tile(frame.reshape(H_orig, W_orig, 1), (1, 1, 3))
+    # H_orig, W_orig = frame.shape[:2]
+    # if len(frame.shape) == 2 or frame.shape[-1] == 1:
+    #     frame = np.tile(frame.reshape(H_orig, W_orig, 1), (1, 1, 3))
 
-    frame = frame.astype(np.float32) / 255
-    frame = cv.resize(frame, (224, 224))  # resize image to network input size
+
+    # frame = frame.astype(np.float32) / 255
+    # img_l = rgb2lab(frame)  # get L from Lab image
+    # img_rs = cv.resize(img_l, (224, 224))  # resize image to network input size
+    # img_l_rs = img_rs - 50  # subtract 50 for mean-centering
 
     net.setInput(cv.dnn.blobFromImage(frame))
-    ab_dec = net.forward()[0, :, :, :].transpose((1, 2, 0))
+    net.forward()
+
+
+def rgb2lab(frame: np.ndarray) -> np.ndarray:
+    # Use of the OpenCV version sometimes leads to hangs
+    y_coeffs = np.array([0.212671, 0.715160, 0.072169], dtype=np.float32)
+    frame = np.where(frame > 0.04045, np.power((frame + 0.055) / 1.055, 2.4), frame / 12.92)
+    y = frame @ y_coeffs.T
+    L = np.where(y > 0.008856, 116 * np.cbrt(y) - 16, 903.3 * y)
+    return L

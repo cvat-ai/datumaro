02ca61d87b2bc47275002e0cc6ea74a65e57a33d
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2024-04-05 16:37:09 +0300

    Fix getting binary mask bbox

--- datumaro/util/mask_tools.py
+++ datumaro/util/mask_tools.py
@@ -367,9 +367,10 @@ def rles_to_mask(rles: Sequence[Union[CompressedRle, Polygon]], width, height) -
 def find_mask_bbox(mask: BinaryMask) -> BboxCoords:
     cols = np.any(mask, axis=0)
     rows = np.any(mask, axis=1)
+    has_pixels = np.any(cols)
     x0, x1 = np.where(cols)[0][[0, -1]]
     y0, y1 = np.where(rows)[0][[0, -1]]
-    return BboxCoords(x0, y0, x1 - x0, y1 - y0)
+    return BboxCoords(x0, y0, x1 - x0 + has_pixels, y1 - y0 + has_pixels)
 
 
 def merge_masks(

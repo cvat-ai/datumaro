a97909d7b1d8599e1fc1d90e709f2e2a74947d74
Dmitrii Lavrukhin <dlavrukhin@yandex.ru>
2024-12-18 19:15:25 +0400

    moving most tests to unit

--- tests/integration/cli/test_video.py
+++ tests/integration/cli/test_video.py
@@ -3,7 +3,7 @@ import os.path as osp
 from unittest import TestCase
 
 from tests.requirements import Requirements, mark_requirement
-from tests.test_video import make_sample_video
+from tests.unit.test_video import make_sample_video
 from tests.utils.test_utils import TestDir
 from tests.utils.test_utils import run_datum as run
 

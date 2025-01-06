49c4beba9f83b024082f266a0777617dcde5275d
Dmitrii Lavrukhin <dlavrukhin@yandex.ru>
2025-01-06 15:18:36 +0400

    removing unused file

--- tests/assets/compat/v0.1/project/.datumaro/plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-from datumaro.components.dataset_base import DatasetItem, SubsetBase
-
-
-class MyBase(SubsetBase):
-    def __iter__(self):
-        yield from [
-            DatasetItem("1"),
-            DatasetItem("2"),
-        ]

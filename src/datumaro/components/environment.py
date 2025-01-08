e300b6e7a0492f6994e3f68aaba890eff2066f1b
Dmitrii Lavrukhin <dlavrukhin@yandex.ru>
2025-01-08 14:52:22 +0400

    renaming components/dataset_generator.py to components/generator.py

--- src/datumaro/components/environment.py
+++ src/datumaro/components/environment.py
@@ -79,7 +79,7 @@ class Environment:
             SubsetBase,
             Transform,
         )
-        from datumaro.components.dataset_generator import DatasetGenerator
+        from datumaro.components.generator import DatasetGenerator
         from datumaro.components.exporter import Exporter
         from datumaro.components.launcher import Launcher
         from datumaro.components.validator import Validator

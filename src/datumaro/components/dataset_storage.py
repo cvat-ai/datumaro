662345b00d37590f9244cfa93b6e5e16978debfb
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2025-01-28 12:21:45 +0200

    Fix formatting

--- src/datumaro/components/dataset_storage.py
+++ src/datumaro/components/dataset_storage.py
@@ -274,9 +274,9 @@ class DatasetStorage(IDataset):
         old_ids = None
         if self._transforms:
             transform = _StackedTransform(
-                source, 
-                self._transforms, 
-                raise_on_malformed_transform=self._raise_on_malformed_transform
+                source,
+                self._transforms,
+                raise_on_malformed_transform=self._raise_on_malformed_transform,
             )
             if transform.is_local:
                 # An optimized way to find modified items:
@@ -707,9 +707,9 @@ class StreamDatasetStorage(DatasetStorage):
     def stacked_transform(self) -> IDataset:
         if self._transforms:
             transform = _StackedTransform(
-                self._source, 
-                self._transforms, 
-                raise_on_malformed_transform=self._raise_on_malformed_transform
+                self._source,
+                self._transforms,
+                raise_on_malformed_transform=self._raise_on_malformed_transform,
             )
             self._drop_malformed_transforms(transform.malformed_transform_indices)
         else:

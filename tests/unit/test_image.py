d1b3036180bd047935f89fbd9fc1caa6eebb1039
Dmitrii Lavrukhin <dlavrukhin@yandex.ru>
2025-02-12 10:54:59 +0400

    syncing tests/unit/test_image.py

--- tests/unit/test_image.py
+++ tests/unit/test_image.py
@@ -1,13 +1,19 @@
+# Copyright (C) 2019-2024 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
 import os.path as osp
 from itertools import product
 from unittest import TestCase
 
 import numpy as np
 import PIL
+import pytest
 
 import datumaro.util.image as image_module
 
-from tests.requirements import Requirements, mark_requirement
+from ..requirements import Requirements, mark_requirement
+
 from tests.utils.test_utils import TestDir
 
 
@@ -107,3 +113,52 @@ class ImageOperationsTest(TestCase):
                 image_module.IMAGE_BACKEND.set(load_backend)
                 img = image_module.load_image(image_path)
                 assert img.shape == (10, 15, 3)
+
+
+class ImageDecodeTest:
+    @pytest.fixture
+    def fxt_img_four_channels(self) -> np.ndarray:
+        return np.random.randint(low=0, high=256, size=(5, 4, 4), dtype=np.uint8)
+
+    @pytest.mark.parametrize(
+        "image_backend", [image_module.ImageBackend.cv2, image_module.ImageBackend.PIL]
+    )
+    def test_decode_image_context(
+        self, fxt_img_four_channels: np.ndarray, image_backend: image_module.ImageBackend
+    ):
+        img_bytes = image_module.encode_image(fxt_img_four_channels, ".png")
+
+        # 3 channels from ImageColorScale.COLOR_BGR
+        with image_module.decode_image_context(
+            image_backend, image_module.ImageColorChannel.COLOR_BGR
+        ):
+            img_decoded = image_module.decode_image(img_bytes)
+            assert img_decoded.shape[-1] == 3
+            assert np.allclose(fxt_img_four_channels[:, :, :3], img_decoded)
+
+        # 3 channels from ImageColorScale.COLOR_RGB
+        with image_module.decode_image_context(
+            image_backend, image_module.ImageColorChannel.COLOR_RGB
+        ):
+            img_decoded = image_module.decode_image(img_bytes)
+            assert img_decoded.shape[-1] == 3
+            assert np.allclose(
+                fxt_img_four_channels[:, :, :3][:, :, ::-1],  # Flip color channels of the fixture
+                img_decoded,
+            )
+
+        # 4 channels from ImageColorScale.UNCHANGED
+        with image_module.decode_image_context(
+            image_backend, image_module.ImageColorChannel.UNCHANGED
+        ):
+            img_decoded = image_module.decode_image(img_bytes)
+            assert img_decoded.shape[-1] == 4
+
+            if image_backend == image_module.ImageBackend.cv2:
+                assert np.allclose(fxt_img_four_channels, img_decoded)
+            else:
+                # PIL will return RGBA, thus we need to correct the fixture
+                to_rgb = fxt_img_four_channels[:, :, :3][:, :, ::-1]
+                fxt_img_four_channels[:, :, :3] = to_rgb
+
+                assert np.allclose(fxt_img_four_channels, img_decoded)

fcb8021b075249b38be5e7aa0073776173fbc3d2
Dmitrii Lavrukhin <dlavrukhin@yandex.ru>
2025-02-13 15:26:05 +0400

    a bit of info on ImageColorChannel.UNCHANGED

--- src/datumaro/util/image.py
+++ src/datumaro/util/image.py
@@ -53,6 +53,8 @@ class ImageColorChannel(Enum):
     """Image color channel
 
     - UNCHANGED: Use the original image's channel (default).
+        be aware that if image is not 1-channel (grayscale)
+        OpenCV will read an image as BGR(A), but PIL will read an image as RGB(A).
     - COLOR_BGR: Use BGR 3 channels
         (it can ignore the alpha channel or convert the gray scale image)
     - COLOR_RGB: Use RGB 3 channels

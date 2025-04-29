dfe99c3c00db54c7640e5d68ab5e8c45a2cd4311
Dmitrii Lavrukhin <dlavrukhin@yandex.ru>
2025-04-29 13:59:20 +0200

    removing KEEPS_SUBSETS_INTACT

--- src/datumaro/components/dataset_storage.py
+++ src/datumaro/components/dataset_storage.py
@@ -706,22 +706,17 @@ class StreamDatasetStorage(DatasetStorage):
         log.debug("This function has no effect on streaming.")
         pass
 
-    def _apply_stacked_transform(self, source: IDataset):
+    @property
+    def stacked_transform(self) -> IDataset:
         if self._transforms:
             transform = _StackedTransform(
-                source,
+                self._source,
                 self._transforms,
                 raise_on_malformed_transform=self._raise_on_malformed_transform,
             )
             self._drop_malformed_transforms(transform.malformed_transform_indices)
         else:
-            transform = source
-
-        return transform
-
-    @property
-    def stacked_transform(self) -> IDataset:
-        transform = self._apply_stacked_transform(self._source)
+            transform = self._source
 
         self._flush_changes = True
         return transform
@@ -754,26 +749,18 @@ class StreamDatasetStorage(DatasetStorage):
         raise NotAvailableError("Drop-in removal is not allowed in streaming.")
 
     def get_subset(self, name: str) -> IDataset:
-        if all(t[0].KEEPS_SUBSETS_INTACT for t in self._transforms):
-            transformed_subset = self._apply_stacked_transform(self._source.get_subset(name))
-            return StreamSubset(transformed_subset, name)
-
-        return StreamSubset(self, name)
+        return self.subsets()[name]
 
     @property
     def subset_names(self):
-        if any(
-            id(t) not in self._transform_ids_for_latest_subset_names
-            and not t[0].KEEPS_SUBSETS_INTACT
-            for t in self._transforms
-        ):
+        if self._transform_ids_for_latest_subset_names != [id(t) for t in self._transforms]:
             self._subset_names = {item.subset for item in self}
             self._transform_ids_for_latest_subset_names = [id(t) for t in self._transforms]
 
         return self._subset_names
 
     def subsets(self) -> Dict[str, IDataset]:
-        return {subset: self.get_subset(subset) for subset in self.subset_names}
+        return {subset: StreamSubset(self, subset) for subset in self.subset_names}
 
     def transform(self, method: Type[Transform], *args, **kwargs) -> None:
         super().transform(method, *args, **kwargs)

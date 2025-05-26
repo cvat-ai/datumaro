8aa80eedbc3c73f13e7227575b9c3c2cf2e3c98a
Dmitrii Lavrukhin <dlavrukhin@yandex.ru>
2025-05-26 13:00:59 +0200

    revert dataset changes

--- src/datumaro/components/dataset.py
+++ src/datumaro/components/dataset.py
@@ -254,10 +254,7 @@ class Dataset(IDataset):
         else:
             from datumaro.components.hl_ops import HLOps
 
-            merged = HLOps.merge(*sources, merge_policy=merge_policy)
-            if merged.is_stream:
-                merged = Dataset(source=merged, env=merged.env)
-            return merged
+            return HLOps.merge(*sources, merge_policy=merge_policy)
 
         return dataset
 

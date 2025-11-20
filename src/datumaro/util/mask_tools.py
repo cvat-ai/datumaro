2d83c779707e599e7df10840f6651a7a532c54e9
Roman Donchenko <roman@cvat.ai>
2025-11-20 13:21:47 +0200

    Relax numpy requirement (#119)
    * Relax numpy requirement
    
    This allows to install Datumaro on Python 3.12. I took the 2.3 bound from
    upstream Datumaro.
    
    * Fix numpy incompatibility
    
    Cherry-picked from open-edge-platform/datumaro@a5b18ac.
    
    ---------
    
    Co-authored-by: Dick Ameln <dick.ameln@intel.com>

--- src/datumaro/util/mask_tools.py
+++ src/datumaro/util/mask_tools.py
@@ -137,8 +137,8 @@ def paint_mask(mask: IndexMask, colormap=None) -> ColorMask:
     if callable(colormap):
         map_fn = colormap
     else:
-        map_fn = lambda c: colormap.get(c, (0, 0, 0))
-    palette = np.array([map_fn(c)[::-1] for c in range(256)], dtype=np.uint8)
+        map_fn = lambda c: colormap.get(c, (-1, -1, -1))
+    palette = np.mod([map_fn(c)[::-1] for c in range(256)], 256).astype(np.uint8)
 
     mask = mask.astype(np.uint8)
     painted_mask = palette[mask].reshape((*mask.shape[:2], 3))
@@ -153,7 +153,7 @@ def remap_mask(mask: ColorMask, map_fn) -> ColorMask:
     """
     check_is_mask(mask)
 
-    return np.array([max(0, map_fn(c)) for c in range(256)], dtype=np.uint8)[mask]
+    return np.mod([map_fn(c) for c in range(256)], 256).astype(np.uint8)[mask]
 
 
 def make_index_mask(

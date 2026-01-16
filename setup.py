c39db48a48fb2cded87d9cdf31722657f99b331f
Maxim Zhiltsov <zhiltsov.max35@gmail.com>
2026-01-16 15:36:10 +0200

    Upgrade python version

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=CORE_REQUIREMENTS,
     extras_require={
         "tf": ["tensorflow"],

ce5809fb71cb6c420c3a686f6852d4832487a8b5
Andrei Strongin <88829364+Pr1ncee@users.noreply.github.com>
2026-06-10 17:38:25 +0400

    Update src/datumaro/plugins/data_formats/coco/base.py
    Co-authored-by: Maxim Zhiltsov <maxim@cvat.ai>

--- src/datumaro/plugins/data_formats/coco/base.py
+++ src/datumaro/plugins/data_formats/coco/base.py
@@ -473,7 +473,8 @@ class _CocoBase(SubsetBase):
         self,
         ann: Dict[str, Any],
         key: str,
-        cls: Union[Type[T], Tuple[Type, ...]],
+        cls: Union[Type[T], Tuple[Type, ...]], 
+        *,
         default: Any = NOTSET,
     ) -> Any:
         value = ann.get(key, NOTSET)
